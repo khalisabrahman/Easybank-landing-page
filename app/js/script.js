acec44b51ccfa014b46a78408330f09fb63aec09
Khalis Ab Rahman <khalisabrahman@gmail.com>
2021-05-25 11:53:16 +0800

    Added overlay animations when hamburger btn is clicked

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,18 @@
-console.log('hello')
+const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay')
+
+btnHamburger.addEventListener('click', function() {
+    console.log('Menu button clicked');
+    if (header.classList.contains('open')) { // Close hamburger menu
+        header.classList.remove('open');
+        overlay.classList.remove('fade-in');
+        overlay.classList.add('fade-out');
+        
+    } else { // Open hamburger menu
+        header.classList.add('open');
+        overlay.classList.remove('fade-out');
+        overlay.classList.add('fade-in');
+        
+    }
+})
