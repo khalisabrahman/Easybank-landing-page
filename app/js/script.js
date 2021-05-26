c9543c396341500a7fa539a879345e7797638d95
Khalis Ab Rahman <khalisabrahman@gmail.com>
2021-05-26 23:39:49 +0800

    Added hero image and hero text. Responsive for both mobile and desktop layout

--- app/js/script.js
+++ app/js/script.js
@@ -1,18 +1,27 @@
+
+const body = document.querySelector('body');
 const btnHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
-const overlay = document.querySelector('.overlay')
+const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
 
-btnHamburger.addEventListener('click', function() {
-    console.log('Menu button clicked');
-    if (header.classList.contains('open')) { // Close hamburger menu
-        header.classList.remove('open');
-        overlay.classList.remove('fade-in');
-        overlay.classList.add('fade-out');
-        
-    } else { // Open hamburger menu
-        header.classList.add('open');
-        overlay.classList.remove('fade-out');
-        overlay.classList.add('fade-in');
-        
-    }
-})
+btnHamburger.addEventListener('click', function () {
+	console.log('Menu button clicked');
+	if (header.classList.contains('open')) {
+		// Close hamburger menu
+		header.classList.remove('open');
+        body.classList.remove('noscroll');
+		fadeElems.forEach(function (element) {
+			element.classList.remove('fade-in');
+			element.classList.add('fade-out');
+		});
+	} else {
+		// Open hamburger menu
+		header.classList.add('open');
+        body.classList.add('noscroll');
+		fadeElems.forEach(function (element) {
+			element.classList.remove('fade-out');
+			element.classList.add('fade-in');
+		});
+	}
+});
